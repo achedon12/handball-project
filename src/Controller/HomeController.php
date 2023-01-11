2bc30b532f9afdf9793a0bdbae79e0bd37f62f62
Léo Deroin <leo.deroin@etu.univ-grenoble-alpes.fr>
2023-01-11 13:02:26 +0100

    add bdd + login/register system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if($this->getUser()) {
+            return $this->render('home/index.html.twig', [
+                'user' => $this->getUser()
+            ]);
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
         ]);
     }
 }
