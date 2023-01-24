0e20326049778083074116a6977cb1300ac93dc0
Achedon12 <leoderoinleo@gmail.com>
2023-01-24 16:51:02 +0100

    add match form

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Matches;
+use App\Form\CreateMatchType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,11 +20,16 @@ class MatchController extends AbstractController
         ]);
     }
 
-    #[Route('/match/show', name: 'app_match_show')]
-    public function show(): Response
+    #[Route('/match/show/{id}', name: 'app_match_show')]
+    public function show(int $id,ManagerRegistry $doctrine): Response
     {
-        return $this->render('match/show.html.twig', [
-        ]);
+        $match = $doctrine->getRepository(Matches::class)->find($id);
+        if($match) {
+            return $this->render('match/show.html.twig', [
+                'match' => $match,
+            ]);
+        }
+        return $this->redirectToRoute('app_match');
     }
 
     #[Route('/match/edit', name: 'app_match_edit')]
@@ -40,9 +47,20 @@ class MatchController extends AbstractController
     }
 
     #[Route('/match/create', name: 'app_match_create')]
-    public function create(): Response
+    public function create(Request $request, ManagerRegistry $doctrine): Response
     {
+        $match = new Matches();
+        $form = $this->createForm(CreateMatchType::class, $match);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $match = $form->getData();
+            $doctrine->getManager()->persist($match);
+            $doctrine->getManager()->flush();
+            return $this->redirectToRoute('app_match_show', ['id' => $match->getId()]);
+        }
         return $this->render('match/create.html.twig', [
+            'form' => $form->createView(),
         ]);
+
     }
 }
