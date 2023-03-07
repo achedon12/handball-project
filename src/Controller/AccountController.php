c693a55d81a2e045ca463f7bfc84cd1c6d764f79
Léo Deroin <leo.deroin@etu.univ-grenoble-alpes.fr>
2023-03-07 16:52:29 +0100

    update account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
@@ -63,14 +64,49 @@ class AccountController extends AbstractController
     }
 
     #[Route('/account/manage/edit/{id}', name: 'app_account_manage_edit')]
-    public function edit(ManagerRegistry $doctrine): Response
+    public function edit(int $id, Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_home');
         }
 
+        $userManager = $doctrine->getManager();
+        $user = $doctrine->getRepository(User::class)->find($id);
+
+        $form = $this->createFormBuilder()
+            ->add('pseudo', null, [
+                'data' => $user->getPseudo()
+            ])
+            ->add('email', null, [
+                'data' => $user->getEmail()
+            ])
+            ->add("password", null, [
+                "required" => false,
+            ])
+            ->add('roles', null, [
+                'data' => $user->getRoles()[0]
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPseudo($form->get('pseudo')->getData());
+            $user->setEmail($form->get('email')->getData());
+            if($form->get('password')->getData() != null) {
+                $hash = $passwordHasher->hashPassword($user, $form->get('password')->getData());
+                $user->setPassword($hash);
+            }
+            $user->setRoles([$form->get('roles')->getData()]);
+            $userManager->persist($user);
+            $userManager->flush();
+
+            return $this->redirectToRoute('app_account_manage');
+        }
+
+
         return $this->render('account/manageAccountEdit.html.twig', [
             "user" => $this->getUser(),
+            'userEdit' => $user,
+            "form" => $form->createView(),
         ]);
     }
 
@@ -89,6 +125,4 @@ class AccountController extends AbstractController
             "user" => $this->getUser(),
         ]);
     }
-
-
 }
