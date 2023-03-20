53a70f74dda4688fb8be02939cc034180d711b9a
Achedon12 <leoderoinleo@gmail.com>
2023-03-20 14:30:52 +0100

    update createMatch

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -10,6 +10,10 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -44,11 +48,60 @@ class MatchController extends AbstractController
     #[Route('/match/create', name: 'app_match_create')]
     public function create(Request $request, ManagerRegistry $doctrine): Response
     {
-        $match = new Matches();
-        $form = $this->createForm(CreateMatchType::class, $match);
+        $form = $this->createFormBuilder()
+            ->add('equipe_locale', EntityType::class, [
+                'class' => Equipes::class,
+                'choice_label' => 'libelle',
+            ])
+            ->add('domicile_exterieur', ChoiceType::class, [
+                    'choices' => [
+                        'Extérieur' => 0,
+                        'Domicile' => 1
+                    ],
+                    'expanded' => true,
+                    'multiple' => false,
+                ]
+            )
+            ->add('equipe_adverse')
+            ->add('hote', TextType::class, [
+                'label' => 'Hôte',
+            ])
+            ->add('date_heure', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('num_semaine', IntegerType::class,
+                [
+                    'attr' => [
+                        'min' => 1,
+                        'max' => 52,
+                    ],
+                    'label' => 'Numéro de la semaine',
+                    'invalid_message' => 'Le numéro de la semaine doit être compris entre 1 et 52',
+
+                ])
+            ->add('num_journee', IntegerType::class,[
+                'attr' => [
+                    'min' => 1,
+                    'max' => 52,
+                ],
+                'label' => 'Numéro de la journée',
+                'invalid_message' => 'Le numéro de la journée doit être compris entre 1 et 52',
+            ])
+            ->add('gymnase', TextType::class,[
+                'label' => 'Gymnase'
+            ])
+            ->getForm();
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $match = $form->getData();
+            $match = new Matches();
+            $match->setHote($form->get('hote')->getData());
+            $match->setEquipeLocale($form->get('equipe_locale')->getData()->getLibelle());
+            $match->setEquipeAdverse($form->get('equipe_adverse')->getData());
+            $match->setDomicileExterieur($form->get('domicile_exterieur')->getData());
+            $match->setDateHeure(new DateTime($form->get('date_heure')->getData()->format('Y-m-d H:i:s')));
+            $match->setNumSemaine($form->get('num_semaine')->getData());
+            $match->setNumJournee($form->get('num_journee')->getData());
+            $match->setGymnase($form->get('gymnase')->getData());
             $doctrine->getManager()->persist($match);
             $doctrine->getManager()->flush();
             return $this->redirectToRoute('app_match_show', [
@@ -56,6 +109,27 @@ class MatchController extends AbstractController
                 "user" => $this->getUser(),
             ]);
         }
+        /*$form = $this->createForm(CreateMatchType::class, $match);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            dd($form,$match);
+        }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $match->setHote($form->get('hote')->getData());
+            $match->setEquipeLocale($form->get('equipe_locale')->getData()->getLibelle());
+            $match->setEquipeAdverse($form->get('equipe_adverse')->getData());
+            $match->setDomicileExterieur($form->get('domicile_exterieur')->getData());
+            $match->setDateHeure(new DateTime($form->get('date_heure')->getData()));
+            $match->setNumSemaine($form->get('num_semaine')->getData());
+            $match->setNumJournee($form->get('num_journee')->getData());
+            $match->setGymnase($form->get('gymnase')->getData());
+            $doctrine->getManager()->persist($match);
+            $doctrine->getManager()->flush();
+            return $this->redirectToRoute('app_match_show', [
+                'id' => $match->getId(),
+                "user" => $this->getUser(),
+            ]);
+        }*/
         return $this->render('match/create.html.twig', [
             'form' => $form->createView(),
             "user" => $this->getUser(),
