7e78e8f9178b0bd38b174afd6e8638306f02cf9f
Achedon12 <leoderoinleo@gmail.com>
2023-03-21 10:27:25 +0100

    posts

--- public/js/index.js
+++ public/js/index.js
@@ -45,3 +45,33 @@ newElement.forEach((element) => {
     });
 });
 
+function setPosts(){
+    removePosts();
+    for(let i = 0; i < posts.length; i++){
+        let a = document.createElement('a');
+        a.classList.add('news-element');
+        a.id = posts[i].id;
+        a.href = '/post/' + posts[i].id;
+        let h3 = document.createElement('h3');
+        h3.classList.add('name');
+        h3.innerHTML = posts[i].author;
+        let p = document.createElement('p');
+        p.classList.add('content');
+        p.innerHTML = posts[i].title;
+        let img = document.createElement('img');
+        img.classList.add('news-element-img');
+        img.src = '/posts/images/' + posts[i].url_photo;
+        a.appendChild(h3);
+        a.appendChild(p);
+        a.appendChild(img);
+        document.querySelector('.news-content').appendChild(a);
+    }
+}
+
+function removePosts(){
+    let elements = document.querySelectorAll('.news-element');
+    elements.forEach(element => {
+        element.remove();
+    });
+}
+
